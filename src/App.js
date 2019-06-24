d5ee04f2f3e250cfae396e41bf47b0122484b75a
AyrtonKrizan <ayrtonkrizan@gmail.com>
2019-06-24 00:30:26 -0300

    chores: Structuring project in folders to organize components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link, Route } from "react-router-dom";
-import * as BooksAPI from './BooksAPI'
-import Shelf from "./Shelf"
-import Search from "./Search"
-import './App.css'
+import * as BooksAPI from './services/BooksAPI'
+import Shelf from "./components/Shelf"
+import Search from "./components/Search"
+import './style/App.css'
 
 class BooksApp extends React.Component {
   /** Constant to control Shelfs **/
